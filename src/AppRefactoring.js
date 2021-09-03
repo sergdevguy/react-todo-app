115ce15f79b03ccb162ed7c4fb0ddc661510a284
Sergdevguy <sergdevguy@gmail.com>
2021-09-03 15:40:54 +0300

    Refactoring. Creat new start point

---
+++ src/AppRefactoring.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AppRefactoring() {
+    return (
+        <div className="todo">
+            <div className="todo__sidebar">
+
+            </div>
+            <div className="todo__tasks">
+                
+            </div>
+        </div>
+    )
+}
+
+export default AppRefactoring;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppRefactoring from './AppRefactoring';
 
 import './index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AppRefactoring />
   </React.StrictMode>,
   document.getElementById('root')
 );
